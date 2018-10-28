8902d18adb9cec54437c2e554ddc6cf5990d0ffd
shohbek <shoh.kesh.92@mail.ru>
2018-10-29 00:45:38 +0900

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -1,3 +1,41 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
+
+X = np.array([[1, 2],             
+              [1.5, 1.8],
+              [5, 8 ],
+              [8, 8],
+              [1, 0.6],
+              [9,11]])     # Our simple dataset 
+
+plt.scatter(X[:,0], X[:,1], s=150)
+plt.show()
+
+colors = 10*["g","r","c","b","k"]
+
+
+clf = K_Means()
+clf.fit(X)
+
+for centroid in clf.centroids:
+    plt.scatter(clf.centroids[centroid][0], clf.centroids[centroid][1],
+                marker="o", color="k", s=150, linewidths=5)
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], marker="x", color=color, s=150, linewidths=5)
+        
+plt.show() #ploting 
+
+
+
+
+
+
+
 class K_Means:
     def __init__(self, k=2, tol=0.001, max_iter=300):
         self.k = k
